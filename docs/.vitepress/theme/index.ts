e7b95e7bdbfd92a5f93b47a7d57f7b74d17407de
zkhssb <z164907681@gmail.com>
2024-08-31 02:16:55 +0800

    fix: window is not defined

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -6,6 +6,9 @@ import { reinjectFooter } from "./beian";
 export default {
   ...Theme,
   enhanceApp: (ctx: EnhanceAppContext) => {
+    if (typeof window == "undefined") {
+      return;
+    }
     ctx.router.onAfterRouteChanged = (to: string) => {
       reinjectFooter();
       for (let i = 0; i < 50; i++) { // 别tm问我为什么,网站重载的时候样式挂不上,就离谱
