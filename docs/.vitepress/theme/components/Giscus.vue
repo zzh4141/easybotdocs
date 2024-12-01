b75b193d3824709f1432cef845af275f5c243bf5
zzh4141 <awww1234eg@163.com>
2024-12-01 09:37:41 +0800

    feat(docs): 添加 Giscus 评论组件
    - 在 docs/.vitepress/theme/index.ts 中引入 Giscus 组件
    - 在 package.json 中添加 @giscus/vue 依赖
    - 更新 package-lock.json 中的相关依赖

---
+++ docs/.vitepress/theme/components/Giscus.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref, watch } from "vue";
+import { useData } from "vitepress";
+const { frontmatter } = useData();
+const { app, theme, isDark } = useData();
+
+import Giscus from "@giscus/vue";
+</script>
+
+<template>
+  <Giscus v-if="frontmatter.comment != false"
+    id="comments"
+    repo="zzh4141/easybotdocs"
+    repoId="R_kgDOMlZ23w"
+    category="Announcements"
+    categoryId="'DIC_kwDOMlZ2384CiB3g"
+    mapping="pathname"
+    strict="0"
+    term="Welcome to @giscus/react component!"
+    reactionsEnabled="1"
+    emitMetadata="0"
+    inputPosition="top"
+    :theme="preferred_color_scheme"
+    lang="zh-CN"
+    loading="lazy"
+  />
+</template>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -6,9 +6,19 @@ import { reinjectFooter } from "./beian";
 import mediumZoom from "medium-zoom";
 import { onMounted, watch, nextTick } from "vue";
 import { useRoute } from "vitepress";
+import { h } from 'vue';
+import DefaultTheme from 'vitepress/theme';
+import Giscus from './components/Giscus.vue'; // 添加了缺失的右引号
 
 export default {
   extends: Theme,
+  
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'doc-after': () => h(Giscus)
+    });
+  }, // 添加了缺失的逗号
+
   enhanceApp: (ctx: EnhanceAppContext) => {
     if (typeof window == "undefined") {
       return;
@@ -25,15 +35,18 @@ export default {
       }
     };
   },
+
   setup() {
     const route = useRoute();
     const initZoom = () => {
       // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' }); // 默认
       mediumZoom(".main img", { background: "var(--vp-c-bg)" }); // 不显式添加{data-zoomable}的情况下为所有图像启用此功能
     };
+
     onMounted(() => {
       initZoom();
     });
+
     watch(
       () => route.path,
       () => nextTick(() => initZoom())
