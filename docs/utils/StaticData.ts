d10b9fc3ba53048a9ee545e64ac69313ec351004
zkhssb <z164907681@gmail.com>
2025-01-01 05:53:17 +0800

    docs update: 新版本和图片模板

--- docs/utils/StaticData.ts
+++ docs/utils/StaticData.ts
@@ -7,7 +7,7 @@ export const Templates = [
         manifest: {
             name: '图片模板套装',
             description: '本模板包含了一些基础的图片模板，你可以根据需要选择使用。',
-            icon: '/market/assets/easybot_image_template_1.png',
+            icon: 'https://alist.saklit.com/p/EasyBot/ImageTemplates/assets/easybot_image_template_1.png',
             author: ['MiuxuE'],
             tag: ['信息'],
             version: "1.0.0",
@@ -15,7 +15,7 @@ export const Templates = [
             links: [
                 {
                     name: "下载",
-                    href: "https://github.com/zkhssb/easybot-image-template"
+                    href: "https://alist.saklit.com/EasyBot/ImageTemplates"
                 }
             ]
         },
