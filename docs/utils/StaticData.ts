509a7164aae057dd64a3dcb07147bbe43fee8380
zkhssb <z164907681@gmail.com>
2025-01-12 13:36:31 +0800

    docs new: 源代码

--- docs/utils/StaticData.ts
+++ docs/utils/StaticData.ts
@@ -14,6 +14,10 @@ export const Templates = [
             content: "/market/easybot_template_concise_kit/",
             links: [
                 {
+                    name: "源代码",
+                    href: "https://github.com/zkhssb/easybot-react-vite-template/tree/main"
+                },
+                {
                     name: "下载",
                     href: "http://dl.inectar.cn/%E5%9B%BE%E7%89%87%E6%A8%A1%E6%9D%BF/%E7%AE%80%E6%B4%81%E6%A8%A1%E6%9D%BF%E5%A5%97%E8%A3%85"
                 }
